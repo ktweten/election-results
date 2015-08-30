479c8d92c9bb36719db56ec838f012152a09dffe
Kelly <kellytweten@gmail.com>
2015-08-29 17:38:17 -0700

    Removing renamed file. Added comments.

--- app/assets/javascripts/ridings.js
+++ app/assets/javascripts/ridings.js
@@ -1,35 +1,38 @@
 (function() {
     'use strict';
-    if (window.riding !== undefined) {
-        draw(window.riding);
+    if (riding !== undefined) {
+        draw(riding);
     }
 
+    // Extract a short version of the party name.
     function short_party_name(candidate) {
         return candidate.party_name.split(/[\s,\/,-]/)[0];
     }
 
-    function add_names(data) {
-        var names = data.candidates.map(short_party_name);
+    // Create a table containing the candidate name, party and vote count.
+    function add_candidate_table(data) {
         var table = d3.select(".name_table");
         var row_num = 0;
 
         data.candidates.forEach(function (candidate) {
             var row = table.append("tr");
-            row.append("td").text(candidate.party_name.replace('/', ' / '));
+            row.append("td").text(candidate.party_name);
             row.append("td").text(candidate.name);
             row.append("td").text(candidate.votes);
         });
-        return names;
     }
 
+    // Draw a chart and table showing the distribution of votes
+    // among candidates and their parties.
     function draw(data) {
         var bounds = {top: 450, right: 450, bottom: 50, left: 50};
         var graph = d3.select("#chart");
-        var names = add_names(data);
+        var names = data.candidates.map(short_party_name);
         var max_votes = d3.max(data.candidates, function(candidate) {
             return candidate.votes
         });
 
+        // Set the scale according to the maximum number of votes cast for any candidate.
         var y = d3.scale.linear()
             .range([bounds.top, bounds.bottom])
             .domain([0, max_votes]);
@@ -46,16 +49,21 @@
             .scale(x)
             .orient("bottom");
 
+        add_candidate_table(data);
+
+        // Append and translate the y axis.
         graph.append("g")
             .attr("class", "y axis")
             .call(yAxis)
             .attr("transform", "translate(" + bounds.left + ", 0)");
 
+        // Append and translate the x axis.
         graph.append("g")
             .attr("class", "x axis")
             .attr("transform", "translate(0, " + bounds.top + ")")
             .call(xAxis);
 
+        // Set each bar according to the candidate's share of the vote.
         graph.selectAll(".bar")
             .data(data.candidates)
             .enter().append("rect")
